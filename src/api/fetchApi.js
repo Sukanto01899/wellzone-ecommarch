03e5b3f214c92d2498c6a612a2dc80513ddce5e2
Sukanto Das <Suknto01899>
2023-11-09 20:52:42 +0600

    texting

--- src/api/fetchApi.js
+++ src/api/fetchApi.js
@@ -3,12 +3,12 @@
 const fetchApi = {};
 
 fetchApi.get =async  (endPoint)=>{
-     const res = await fetch(`https://wellzonebd.onrender.com/${endPoint}`);
+     const res = await fetch(`https://wellzonebd.onrender.com/api/${endPoint}`);
      const data = res.json();
      return data;
 }
 fetchApi.post =async (endPoint, body)=>{
-     const res =await fetch(`https://wellzonebd.onrender.com/${endPoint}`, {
+     const res =await fetch(`https://wellzonebd.onrender.com/api/${endPoint}`, {
         method: "POST",
         credentials: "include",
         headers: {
--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -3,6 +3,7 @@ import fetchApi from "../api/fetchApi";
 
 const useProducts = ()=>{
     const {isLoading, error, data:products} = useQuery({queryKey: ['products'], queryFn: ()=> fetchApi.get('product')})
+    console.log(products)
     return [products, isLoading, error]
 }
 
